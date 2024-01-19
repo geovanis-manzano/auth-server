0d7b1a68ac9bb2987c35c45302050c410cfecfe8
geovanis-manzano <gmanzanochacon@gmail.com>
2024-01-19 17:04:48 +0100

    first commit

---
+++ src/test/java/com/gmanzano/springboot/security/authserver/AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmanzano.springboot.security.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
